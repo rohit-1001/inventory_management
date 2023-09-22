79d207094f559f82b588bce0ecce1d067b6d8f5b
rohit-1001 <114739041+rohit-1001@users.noreply.github.com>
2023-09-23 01:29:49 +0530

    server vendor middleware

---
+++ server/middleware/vendorAuthenticate.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const Vendor = require('../models/Vendor')
+
+
+const Authenticate = async (req, res, next) => {
+    try {
+        const token = req.cookies.libcoo;
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY)
+        
+        const findVendor = await Vendor.findOne({_id:verifyToken._id, "tokens.token":token})
+
+        if(!findVendor){
+            throw new Error("Login Expired")
+        }
+
+        req.token=token
+        req.findVendor=findVendor
+        req.userID=findVendor._id
+        next()
+    } catch (error) {
+        res.status(401).json({msg:'Unauthorized access'})
+    }
+}
+
+module.exports = vendorAuthenticate

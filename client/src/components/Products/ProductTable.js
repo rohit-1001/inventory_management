30b265a6586384b2526ea072dcc3b792c2648030
adwaitp <adwait.purao@spit.ac.in>
2023-09-28 15:54:59 +0530

    Improved the UI of Products page

---
+++ client/src/components/Products/ProductTable.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { DataGrid, GridToolbarContainer, GridToolbarExport, GridToolbarDensitySelector } from '@mui/x-data-grid';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    {
+        field: 'date',
+        headerName: 'Date',
+        type: 'numeric',
+        width: 200,
+    },
+    { field: 'vendor', headerName: 'Vendor', width: 200 },
+    { field: 'productName', headerName: 'Product Name', width: 200 },
+];
+
+
+function CustomToolbar() {
+    return (
+        <GridToolbarContainer>
+            <GridToolbarExport />
+            <GridToolbarDensitySelector />
+            <IconButton color="primary" aria-label="delete selected rows" component="span">
+                <DeleteIcon />
+            </IconButton>
+        </GridToolbarContainer>
+    );
+}
+
+export default function ProductTable(props) {
+    const [selectionModel, setSelectionModel] = React.useState([]);
+
+    const handleSelection = (newSelection) => {
+        setSelectionModel(newSelection.selectionModel);
+    };
+
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={props.data}
+                columns={columns}
+                components={{
+                    Toolbar: CustomToolbar,
+                }}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                onSelectionModelChange={handleSelection}
+                selectionModel={selectionModel}
+                pagination
+                pageSize={5}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+            />
+        </div>
+    );
+}
--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
-import ProductsFilter from "./ProductsFilter";
 import "./Products.css";
-import ProductsList from "./ProductsList";
+import ProductTable from "./ProductTable";
 
-const Products = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+const rows = [
+  { id: 1, date: '2019-09-09', vendor: 'Jon', productName: 'Shampoo' },
+  { id: 2, date: '2020-03-15', vendor: 'Alice', productName: 'Soap' },
+  { id: 3, date: '2020-06-22', vendor: 'Bob', productName: 'Toothpaste' },
+  { id: 4, date: '2020-11-18', vendor: 'Emily', productName: 'Body Lotion' },
+  { id: 5, date: '2021-02-05', vendor: 'David', productName: 'Conditioner' },
+  { id: 6, date: '2021-08-30', vendor: 'Sophia', productName: 'Face Wash' },
+  { id: 7, date: '2022-12-10', vendor: 'Olivia', productName: 'Hand Sanitizer' },
+  { id: 8, date: '2023-07-25', vendor: 'Liam', productName: 'Shower Gel' },
+  { id: 9, date: '2023-05-14', vendor: 'Emma', productName: 'Perfume' },
+];
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
-
-  const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
-
-  
+const Products = () => {
   return (
     <div>
-      <Card className="expenses">
-        <ProductsFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-       <ProductsList items={filteredExpenses}/>
+      <Card className="products">
+       <ProductTable data={rows} />
       </Card>
     </div>
   );

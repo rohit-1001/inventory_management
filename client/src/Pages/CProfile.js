85f028cabd304a489e006a82c9280ebf9d9f345c
akash <akashpanicker17@gmail.com>
2023-10-17 22:15:32 +0530

    Added Edit Option in Profile Page.

--- client/src/Pages/CProfile.js
+++ client/src/Pages/CProfile.js
@@ -1,73 +1,111 @@
-import React from 'react';
-import { Container, Grid, Paper, Avatar, Typography, Table, TableContainer, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
-import image1 from '../assets/profile123.jpg';
+import React, { useState } from 'react';
+import { Container, Grid, Paper, Typography, Button, Modal, Box, TextField } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import image1 from '../assets/testi1.jpg';
+
 function CProfile() {
   // Sample data for company info
-  const companyInfo = {
+  const [companyInfo, setCompanyInfo] = useState({
     name: 'Sample Company',
     email: 'sample@email.com',
     contactNumber: '+1234567890',
-    logo: {image1},
+    logo: image1,
+    password: '123',
+  });
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
   };
 
-  // Sample data for past orders
-  const pastOrders = [
-    { orderID: 1, productName: 'Product A', dateOrdered: '2023-09-24', quantity: 10 },
-    { orderID: 2, productName: 'Product B', dateOrdered: '2023-09-23', quantity: 5 },
-    { orderID: 3, productName: 'Product C', dateOrdered: '2023-09-22', quantity: 8 },
-  ];
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
-  console.log('Company Info:', companyInfo);
-  console.log('Past Orders:', pastOrders);
+  const handleSubmit = () => {
+    // Handle the form submission here
+    // You can update the companyInfo state and perform any other necessary actions
+    handleCloseModal();
+  };
 
   return (
     <>
-    <br></br>
-    <Container maxWidth="lg">
-      <Grid container spacing={3}>
-        {/* Company Profile */} 
-        <Grid item xs={12}>
-          <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
-            <Avatar alt="Company Logo" src={companyInfo.logo} style={{ width: '100px', height: '100px', margin: '0 auto' }} />
-            <Typography variant="h5">{companyInfo.name}</Typography>
-            <Typography variant="subtitle1">Email: {companyInfo.email}</Typography>
-            <Typography variant="subtitle1">Contact Number: {companyInfo.contactNumber}</Typography>
-          </Paper>
+      <br />
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          {/* Company Profile */}
+          <Grid item xs={12}>
+            <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
+              <div>
+                <img
+                  src={companyInfo.logo}
+                  alt="Company Logo"
+                  style={{ width: '100px', height: '100px', borderRadius: '50%', marginBottom: '10px' }}
+                />
+              </div>
+              <Typography variant="h5">{companyInfo.name}</Typography>
+              <Typography variant="subtitle1">Email: {companyInfo.email}</Typography>
+              <Typography variant="subtitle1">Contact Number: {companyInfo.contactNumber}</Typography>
+              <Typography variant="subtitle1">Password: {companyInfo.password}</Typography>
+              <Button variant="contained" color="primary" onClick={handleOpenModal} startIcon={<EditIcon />}>
+                Edit
+              </Button>
+            </Paper>
+          </Grid>
         </Grid>
-
-        {/* Past Orders */}
-        <Grid item xs={12}>
-          <Paper elevation={3} style={{ padding: '20px' }}>
-            <Typography variant="h4" style={{ marginBottom: '20px' }}>Past Orders</Typography>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Order ID</TableCell>
-                    <TableCell>Product Name</TableCell>
-                    <TableCell>Date Ordered</TableCell>
-                    <TableCell>Quantity</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {pastOrders.map((order) => (
-                    <TableRow key={order.orderID}>
-                      <TableCell>{order.orderID}</TableCell>
-                      <TableCell>{order.productName}</TableCell>
-                      <TableCell>{order.dateOrdered}</TableCell>
-                      <TableCell>{order.quantity}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      {/* Modal for editing company info */}
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h6" gutterBottom>
+            Edit Company Info
+          </Typography>
+          <form>
+            <TextField
+              label="Name"
+              fullWidth
+              value={companyInfo.name}
+              onChange={(e) => setCompanyInfo({ ...companyInfo, name: e.target.value })}
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Email"
+              fullWidth
+              value={companyInfo.email}
+              onChange={(e) => setCompanyInfo({ ...companyInfo, email: e.target.value })}
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Contact Number"
+              fullWidth
+              value={companyInfo.contactNumber}
+              onChange={(e) => setCompanyInfo({ ...companyInfo, contactNumber: e.target.value })}
+            />
+            <br></br>
+            <br></br>
+            <br></br>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </>
   );
-  
 }
 
 export default CProfile;

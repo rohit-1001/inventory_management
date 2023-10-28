6563fc5a6e3267f014cda4d51bf2ebd6d04ced7c
akash <akashpanicker17@gmail.com>
2023-10-28 14:36:00 +0530

    Profile Page - Fields Added.

--- client/src/Pages/CProfile.js
+++ client/src/Pages/CProfile.js
@@ -6,25 +6,25 @@ import axios from 'axios'
 
 function CProfile() {
   const [user, setUser] = useState({
-    name:"",
-    email:"",
-    phone:null
+    name: "",
+    email: "",
+    phone: null
   })
   const [newuser, setNewUser] = useState({
-    name:"",
-    email:"",
-    phone:null
+    name: "",
+    email: "",
+    phone: null
   })
   const getUserInfo = async () => {
     try {
       const c = await axios.get('/profile', {
-        withCredentials:true
+        withCredentials: true
       })
-      const {name, email, phone} = c.data
-      setUser({name, email, phone})
-      setNewUser({name, email, phone})
+      const { name, email, phone } = c.data
+      setUser({ name, email, phone })
+      setNewUser({ name, email, phone })
     } catch (error) {
-      if(error.response){
+      if (error.response) {
         alert(error.response.data.error)
       }
     }
@@ -35,7 +35,7 @@ function CProfile() {
     document.title = 'Sangrah | Profile';
     getUserInfo()
   }, []);
-  
+
   const [companyInfo, setCompanyInfo] = useState({
     name: 'Sample Company',
     email: 'sample@email.com',
@@ -44,11 +44,14 @@ function CProfile() {
     companyGenre: 'Food', // Add company genre
     logo: image1,
     password: '123',
+    GSTNO: 3000423,
+    Grole: "Vendor",
+    dob: "01/01/2000",
   });
 
   const [openModal, setOpenModal] = useState(false);
 
-  const handleOpenModal = async() => {
+  const handleOpenModal = async () => {
     setOpenModal(true);
   };
 
@@ -58,15 +61,15 @@ function CProfile() {
 
   const handleSubmit = async () => {
     try {
-      const c = await axios.post('/updateprofile',{name:newuser.name, email:newuser.email, phone:newuser.phone}, {
-        withCredentials:true
+      const c = await axios.post('/updateprofile', { name: newuser.name, email: newuser.email, phone: newuser.phone }, {
+        withCredentials: true
       })
 
-      if(c.status===200){
+      if (c.status === 200) {
         alert(c.data.msg)
       }
     } catch (error) {
-      if(error.response){
+      if (error.response) {
         alert(error.response.data.error)
       }
     }
@@ -81,21 +84,31 @@ function CProfile() {
         <Grid container spacing={3}>
           {/* Company Profile */}
           <Grid item xs={12}>
-            <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
-              <div>
+            <Paper elevation={3} style={{ padding: '60px', textAlign: 'center', display: "flex" }}>
+              <div style={{ width: "100%", margin: "0 auto" }}>
                 <img
                   src={companyInfo.logo}
                   alt="Company Logo"
-                  style={{ width: '100px', height: '100px', borderRadius: '50%', marginBottom: '10px' }}
+                  style={{ width: '250px', height: '250px', borderRadius: '50%', marginBottom: '10px' }}
                 />
               </div>
-              <Typography variant="h5">{user.name}</Typography>
-              <Typography variant="subtitle1">Email: {user.email}</Typography>
-              <Typography variant="subtitle1">Contact Number: {user.phone}</Typography>
+              <div style={{ width: "100%", margin: "0 auto", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+                <Typography variant="h5">{user.name}</Typography>
+                <Typography variant="subtitle1">Email: {user.email}</Typography>
+                <Typography variant="subtitle1">Contact Number: {user.phone}</Typography>
+                <Typography variant="subtitle1">Address: {companyInfo.address}</Typography>
+                <Typography variant="subtitle1">Company Type: {companyInfo.companyGenre}</Typography>
+                <Typography variant="subtitle1">GST No: {companyInfo.GSTNO}</Typography>
+                <Typography variant="subtitle1">Role: {companyInfo.Grole}</Typography>
+                <Typography variant="subtitle1">Date of Birth: {companyInfo.dob}</Typography>
+              </div>
+            </Paper>
+            <br></br>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <Button variant="contained" color="primary" onClick={handleOpenModal} startIcon={<EditIcon />}>
                 Edit
               </Button>
-            </Paper>
+            </div>
           </Grid>
         </Grid>
       </Container>
@@ -107,15 +120,18 @@ function CProfile() {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: 400,
+            maxWidth: '90%', // Adjust the maximum width as needed
             bgcolor: 'background.paper',
             boxShadow: 24,
             p: 4,
+            overflowY: 'auto', // Enable vertical scrolling
+            maxHeight: '90vh', // Limit the maximum height to the viewport height
           }}
         >
           <Typography variant="h6" gutterBottom>
             Edit Company Info
           </Typography>
+          <br></br>
           <form>
             <TextField
               label="Name"
@@ -138,6 +154,45 @@ function CProfile() {
               fullWidth
               value={newuser.phone}
               onChange={(e) => setNewUser({ ...newuser, phone: e.target.value })}
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Address"
+              fullWidth
+              value={companyInfo.address}
+
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Company Type"
+              fullWidth
+              value={companyInfo.companyGenre}
+
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="GST No"
+              fullWidth
+              value={companyInfo.GSTNO}
+              disabled
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Role"
+              fullWidth
+              value={companyInfo.Grole}
+              disabled
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              label="Date of Birth"
+              fullWidth
+              value={companyInfo.dob}
             />
             <br></br>
             <br></br>

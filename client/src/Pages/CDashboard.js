f44b068776d1bc65cfd5e24af9d273ff80ea7ff9
adwaitp <adwait.purao@spit.ac.in>
2023-11-01 08:25:51 +0530

    Integrated line chart

--- client/src/Pages/CDashboard.js
+++ client/src/Pages/CDashboard.js
@@ -3,31 +3,34 @@ import UpperBoxes from '../components/UpperBoxes';
 import StockAlert from '../components/StockAlert';
 import Compchta from '../components/Compchta';
 import { useEffect } from 'react';
-import BarChart from '../components/BarChart';
+// import BarChart from '../components/BarChart';
+import LineChart from '../components/LineChart';
+
 const CDashboard = (props) => {
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     document.title = 'Sangrah | Dashboard';
   }, [])
-  const chartData = {
-    labels: ['Product A', 'Product B', 'Product C'],
-    datasets: [
-      {
-        label: 'Selling Price',
-        data: [50, 60, 40],
-        backgroundColor: 'rgba(54, 162, 235, 0.2)', // A shade of blue
-        borderColor: 'rgba(54, 162, 235, 1)', // A shade of blue
-        borderWidth: 1,
-      },
-      {
-        label: 'Manufacturing Price',
-        data: [30, 40, 25],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)', // Another shade of blue
-        borderColor: 'rgba(75, 192, 192, 1)', // Another shade of blue
-        borderWidth: 1,
-      },
-    ],
-  };
+  // const chartData = {
+  //   labels: ['Product A', 'Product B', 'Product C'],
+  //   datasets: [
+  //     {
+  //       label: 'Selling Price',
+  //       data: [50, 60, 40],
+  //       backgroundColor: 'rgba(54, 162, 235, 0.2)', // A shade of blue
+  //       borderColor: 'rgba(54, 162, 235, 1)', // A shade of blue
+  //       borderWidth: 1,
+  //     },
+  //     {
+  //       label: 'Manufacturing Price',
+  //       data: [30, 40, 25],
+  //       backgroundColor: 'rgba(75, 192, 192, 0.2)', // Another shade of blue
+  //       borderColor: 'rgba(75, 192, 192, 1)', // Another shade of blue
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
+
   const name = "Prices";
   const parentContainerStyle = {
     display: "flex",
@@ -58,7 +61,8 @@ const CDashboard = (props) => {
           <UpperBoxes details={{ role: props.details.role }} />
         </div>
         <div style={chartContainerStyle}>
-          <BarChart data={chartData} name={name} />
+          {/* <BarChart data={chartData} name={name} /> */}
+          <LineChart details={{ role: props.details.role}} />
         </div>
       </div>
       <StockAlert details={{ role: props.details.role }} />
--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,39 +1,98 @@
-import React from 'react';
+import { React, useState } from 'react'
 import { Line } from 'react-chartjs-2';
+import axios from 'axios'
+import { useEffect } from 'react';
 
-const dummyData = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-  datasets: [
-    {
-      label: 'Example Data',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(30, 144, 255, 0.4)', // A shade of blue
-      borderColor: 'rgba(30, 144, 255, 1)', // A shade of blue
-      pointBorderColor: 'rgba(30, 144, 255, 1)', // A shade of blue
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(30, 144, 255, 1)', // A shade of blue
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55],
-    },
-  ],
-};
 
-function LineChart() {
+function LineChart(props) {
+  const initialData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        label: `${props.details.role} data`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(30, 144, 255, 0.4)', // A shade of blue
+        borderColor: 'rgba(30, 144, 255, 1)', // A shade of blue
+        pointBorderColor: 'rgba(30, 144, 255, 1)', // A shade of blue
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(30, 144, 255, 1)', // A shade of blue
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55],
+      },
+    ],
+  };
+
+  const [lineChartData, setLineChartData] = useState(initialData);
+  const [months, setMonths] = useState([]);
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    if (props.details.role === "vendor") {
+      const lineChart = axios.get('/monthlysales_v').then((res) => {
+        const salesData = res.data;
+        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        setMonths(salesData.map(entry => monthNames[entry.month - 1])); // subtract 1 because array indices start at 0
+        setSales(salesData.map(entry => entry.sales));
+
+        if (months.length === 0) {
+          months.push(['January', 'February', 'March', 'April', 'May', 'June']);
+          sales.push([65, 59, 80, 81, 56, 55]);
+        }
+
+        let chartData = {
+          ...initialData,
+          labels: months,
+          datasets: initialData.datasets.map(dataset => ({
+            ...dataset,
+            data: sales,
+          })),
+        };
+
+        setLineChartData(chartData)
+      })
+    }
+
+    else if (props.details.role === "company") {
+      const lineChart = axios.get('/monthlysales_c').then((res) => {
+        const salesData = res.data;
+        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        setMonths(salesData.map(entry => monthNames[entry.month - 1])); // subtract 1 because array indices start at 0
+        setSales(salesData.map(entry => entry.sales));
+
+        if (months.length === 0) {
+          months.push(['January', 'February', 'March', 'April', 'May', 'June']);
+          sales.push([65, 59, 80, 81, 56, 55]);
+        }
+
+        let chartData = {
+          ...initialData,
+          labels: months,
+          datasets: initialData.datasets.map(dataset => ({
+            ...dataset,
+            data: sales,
+          })),
+        };
+
+        setLineChartData(chartData)
+      })
+    }
+  }, [])
+
   return (
-    <div style={{ margin: 'auto', textAlign: 'center', width:"500px",margin:"1em", height:"300%" }}>
+    <div style={{ margin: 'auto', textAlign: 'center', width: "500px", margin: "1em", height: "300%" }}>
       <Line
-        data={dummyData}
+        data={lineChartData}
         options={{
           scales: {
             y: {
